d91f2a9921f851cd3bdbe869a52fafd89d56e25b
Grace Fox <grace.fox@xero.com>
2018-03-30 18:36:12 +1300

    refactor
    and remove unnecessary file

--- script.js
+++ script.js
@@ -2,23 +2,31 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
-button.addEventListener("click", function() {
-    if (input.value.length > 0) {  //dont allow null
-        var li = document.createElement("li");
-        li.appendChild(document.createTextNode(input.value));
-        ul.appendChild(li);
-        input.value = ""; //clears input box
-        console.log("click is working");
-    }
-})
+function inputLength() {
+    return input.value.length;
+}
 
-var enterkey = 13;
-input.addEventListener("keypress", function(event) {
-    if (input.value.length > 0 && event.keyCode === enterkey) {
-        var li = document.createElement("li");
+function createListElement() {
+    var li = document.createElement("li");
         li.appendChild(document.createTextNode(input.value));
         ul.appendChild(li);
         input.value = "";
+}
+
+function addListAfterClick() {
+    if (inputLength() > 0) {  //dont allow null
+        createListElement();
     }
-    
-})
+}
+
+var enterkey = 13;
+function addListAfterKeypress(event) {
+    if (inputLength() > 0 && event.keyCode === enterkey) {
+        createListElement();
+    }
+}
+
+
+button.addEventListener("click", addListAfterClick();
+
+input.addEventListener("keypress", addListAfterKeypress();
